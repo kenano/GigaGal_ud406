65939990ed52ef8810d721539e573642ef29fd70
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-06-02 16:47:03 -0700

    - Create the Assets class. It manages all of the assets used.

--- core/src/com/kenan/libgdxtutorial/gigagal/util/Assets.java
+++ core/src/com/kenan/libgdxtutorial/gigagal/util/Assets.java
@@ -1,7 +1,85 @@
 package com.kenan.libgdxtutorial.gigagal.util;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+
+
 /**
  * Created by KenanO on 6/1/16.
+ * This utility class holds onto all the assets used. It's a singleton, so the constructor is
+ * private, and a single instance is created when the class is loaded. That way all
+ * the entities that make up the game can access their sprites in the same place, and no work
+ * loading up textures is repeated.
+ *
+ * Each entity gets its own inner class to hold its assets. Below you'll complete the GigaGalAssets
+ * class by finding up the standing-right AtlasRegion within the TextureAtlas loaded in init() .
  */
-public class Assets {
+public class Assets implements Disposable, AssetErrorListener {
+
+    public static final String TAG = Assets.class.getName();
+
+    /** an instance of this class. Since its a singleton it can be static. */
+    public static final Assets assetsInstance = new Assets();
+
+    /**
+     * assets for the gigagal sprite. since this class is a singleton we can give public access
+     * to this member since there is always be one instance of it.
+     */
+    public GigaGalAssets gigaGalAssets;
+
+    // AssetManager - Loads and stores assets like textures, bitmapfonts, tile maps, etc
+    private AssetManager mAssetManager;
+
+    /*
+     * Constructor
+     * Since this class should be instantiated as a singleton make its constructor private so no
+     * other class can instantiate a second instance.
+     */
+    private Assets() {}
+
+    /**Create an instance of mAssetManager, sets a ErrorListener to it, loads texture atlas. */
+    public void init() {
+
+        mAssetManager = new AssetManager();
+        mAssetManager.setErrorListener(this);
+        mAssetManager.load(Constants.TEXTURE_ATLAS, TextureAtlas.class);
+        mAssetManager.finishLoading();
+
+        //get the textureatlas from the AssetManager
+        TextureAtlas atlas = mAssetManager.get(Constants.TEXTURE_ATLAS);
+
+        //create an instance of the  GigaGalAssests obj.
+        gigaGalAssets = new GigaGalAssets(atlas);
+    }
+
+    /** This inner class stores the assets for the Gigagal entity.*/
+    public class GigaGalAssets {
+
+        //holds the standing-right sprite
+        private TextureAtlas.AtlasRegion mAtlasRegionStandingRight;
+
+        /**
+         *
+         * @param atlas The texture altas which has the image assets for the project.
+         */
+        public GigaGalAssets(TextureAtlas atlas) {
+
+            //initialize the standing right AtlasRegion
+            mAtlasRegionStandingRight = atlas.findRegion(Constants.SPRITE_STANDING_RIGHT_NAME);
+        }
+    }
+
+    @Override
+    public void error(AssetDescriptor asset, Throwable throwable) {
+        Gdx.app.error(TAG, "Couldn't load asset: " + asset.fileName, throwable);
+    }
+
+    @Override
+    public void dispose() {
+        mAssetManager.dispose();
+    }
 }
--- core/src/com/kenan/libgdxtutorial/gigagal/util/Constants.java
+++ core/src/com/kenan/libgdxtutorial/gigagal/util/Constants.java
@@ -1,7 +1,23 @@
 package com.kenan.libgdxtutorial.gigagal.util;
 
+import com.badlogic.gdx.graphics.Color;
+
 /**
  * Created by KenanO on 6/1/16.
  */
 public class Constants {
+
+    //the background color used.
+    public static final Color BACKGROUND_COLOR = Color.SKY;
+
+    // the world size. the number of pixels of our Pixel art that will fit on the screen.
+    // use this size to initialize both dimensions of an ExtendViewport
+    // the game will be run in landscape mode, so this will specify the height of the world.
+    public static final float WORLD_SIZE = 128.0f;
+
+    //name of label from .atlas for sprite: player standing facing to the right.
+    public static final String SPRITE_STANDING_RIGHT_NAME = "standing-right";
+
+    //path to file which defines the texture atlas.
+    public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
 }
