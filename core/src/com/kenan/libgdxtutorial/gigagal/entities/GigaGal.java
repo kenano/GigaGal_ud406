85318a6f4bcef2175935da93aae6d34ff7ab009b
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-06-05 14:17:27 -0700

    - Added code to move GigaGal left and right using the arrow keys. - added javadoc.

--- core/src/com/kenan/libgdxtutorial/gigagal/entities/GigaGal.java
+++ core/src/com/kenan/libgdxtutorial/gigagal/entities/GigaGal.java
@@ -1,6 +1,9 @@
 package com.kenan.libgdxtutorial.gigagal.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.kenan.libgdxtutorial.gigagal.util.Assets;
 import com.kenan.libgdxtutorial.gigagal.util.Constants;
@@ -27,38 +30,65 @@ public class GigaGal {
 
     /**
      *
-     * update Gigagals properties.
+     * update Gigagals properties. Update position of GigaGal based on result of
+     * Gdx.input.isKeyPressed.
      *
      * @param delta time difference since game has started.
      */
     public void update(float delta) {
 
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            moveLeft(delta);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            moveRight(delta);
+        }
     }
 
     /**
      *
-     * @param batch
+     * draw the spritebatch for GigaGal.
+     *
+     * @param batch time difference since game has started.
      */
     public void render(SpriteBatch batch) {
 
+        TextureRegion texture_region_standing_right =
+                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight;
+
         //Draw the standing right AtlasRegion at GGs current position
         batch.draw(
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getTexture(),
+                texture_region_standing_right.getTexture(),
                 mPosition.x - Constants.VECTOR_GIGAGAL_EYE_POSITION.x,
                 mPosition.y - Constants.VECTOR_GIGAGAL_EYE_POSITION.y,
                 0,
                 0,
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionWidth(),
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionHeight(),
+                texture_region_standing_right.getRegionWidth(),
+                texture_region_standing_right.getRegionHeight(),
                 1,
                 1,
                 0,
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionX(),
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionY(),
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionWidth(),
-                Assets.assetsInstance.gigaGalAssets.atlasRegionStandingRight.getRegionHeight(),
+                texture_region_standing_right.getRegionX(),
+                texture_region_standing_right.getRegionY(),
+                texture_region_standing_right.getRegionWidth(),
+                texture_region_standing_right.getRegionHeight(),
                 false,
                 false);
+
+    }
+
+    private void moveLeft(float delta) {
+        //update GGs position based on GGs speed from Constants and the change rate delta parameter.
+
+        mPosition.x -= Constants.FLOAT_GIGAGAL_SPEED * delta;
+
+    }
+
+    private void moveRight(float delta) {
+        //update GGs position based on GGs speed from Constants and the change rate delta parameter.
+
+        mPosition.x += Constants.FLOAT_GIGAGAL_SPEED * delta;
 
     }
 
--- core/src/com/kenan/libgdxtutorial/gigagal/util/Constants.java
+++ core/src/com/kenan/libgdxtutorial/gigagal/util/Constants.java
@@ -8,23 +8,26 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class Constants {
 
-    //the background color used.
+    /**the background color used. */
     public static final Color BACKGROUND_COLOR = Color.SKY;
 
-    // the world size. the number of pixels of our Pixel art that will fit on the screen.
-    // use this size to initialize both dimensions of an ExtendViewport
-    // the game will be run in landscape mode, so this will specify the height of the world.
+    /** the world size. the number of pixels of our Pixel art that will fit on the screen.
+     * use this size to initialize both dimensions of an ExtendViewport
+     * the game will be run in landscape mode, so this will specify the height of the world. */
     public static final float WORLD_SIZE = 128.0f;
 
-    //name of label from .atlas for sprite: player standing facing to the right.
+    /** name of label from .atlas for sprite: player standing facing to the right. */
     public static final String SPRITE_STANDING_RIGHT_NAME = "standing-right";
 
-    //path to file which defines the texture atlas.
+    /** path to file which defines the texture atlas. */
     public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
 
-    //A Vector2 Constant for GigaGal's eye position within her sprites.
+    /** A Vector2 Constant for GigaGal's eye position within her sprites.*/
     public static final Vector2 VECTOR_GIGAGAL_EYE_POSITION = new Vector2(16,24);
 
-    //A float constant for the height of GigaGal's eye above her feet.
+    /** A float constant for the height of GigaGal's eye above her feet. */
     public static final float FLOAT_GIGAGAL_FEET_TO_EYES_DIST = 16.0f;
+
+    /** a float for GigaGal's movement speed */
+    public static final float FLOAT_GIGAGAL_SPEED = 72.0f;
 }
